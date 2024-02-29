c0fc6ed9d22e13d174c5cdd5658f121ed33e53d8
FABIO BUGANZA GRACCE <fabio.bgracce@senacsp.edu.br>
2024-02-28 22:19:16 -0300

    Primeiro Commit

---
+++ ExerciciosTi20N/Control.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosTi20N
+{
+     class Control
+    {
+        public Control() 
+        { 
+        
+        
+        
+        }//fim do construtor
+
+        public void Manu()
+
+        {
+
+            Console.WriteLine("\n\nEScolha uma das opçoes abaixo: \n" +
+                "0. Sair \n"                                          +
+                "1. Exercicio 01\n"                                   +
+                "2. Exercicio 02\n"                                   +
+                "3. Exercicio 03\n"                                   +
+                "4. Exercicio 04\n");
+        
+        
+        }//fim do menu
+
+
+    }//fim da classe
+}//fim do projeto
---
+++ ExerciciosTi20N/Model.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosTi20N
+{
+     class Model
+    {
+        //Construtor
+
+        public Model() 
+        { 
+        
+        
+        
+        }//fim do metado construtor
+
+        // Exercicio 01: Faça um programa que imprima os numeros de 1 a 10.
+
+        public string ExercicioUm()
+        {
+
+            string resultado = "";
+            for(int i=0; i < 10; i++) 
+            {
+
+                resultado += "\n" + i+1;
+            
+            }
+
+            return resultado;   
+        
+        }//fim do metodo
+
+
+    }//fim da classe
+}//fim do projeto
